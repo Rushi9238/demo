41acdfa0e36c76f531eaa5ae660f566eb7537236
Rushikesh Patil <113586690+Rushi9238@users.noreply.github.com>
2024-04-30 15:07:27 +0530

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-import {Route,Routes} from 'react-router-dom'
+import {Route,Routes, BrowserRouter as Router} from 'react-router-dom'
 import Home from '../Page/Home'
 import About from '../Page/About'
 import Contact from '../Page/Contact'
 import PageNotFound from '../Page/PageNotFound'
+import {BrowserRouter} from 'react-router-dom'
 function App() {
 
   return (
     <>
+      <Router>
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='*' element={<PageNotFound/>}/>
    </Routes>
+  </Router>
+  
     </>
   )
 }
